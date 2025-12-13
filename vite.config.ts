087db1f6376babfba410fa3c7a8a7f9d60512022
mwj <892790756@qq.com>
2025-12-13 14:19:29 +0800

    更新打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import viteCompression from 'vite-plugin-compression'
 const pathSrc = path.resolve(__dirname, "src");
 export default defineConfig((mode): any =>{
   const env = loadEnv(mode.mode, process.cwd());
@@ -23,6 +27,19 @@ export default defineConfig((mode): any =>{
     },
     plugins: [
       vue(),
+      AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240, // 大于10kb的文件进行压缩
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
     ],
     resolve: {
       alias: {
